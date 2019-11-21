be1daed6c3ab7e10ffd325b17e0e29c5ef2c190d
Matthew Jones <m110.jones@student.qut.edu.au>
2019-11-21 15:50:52 +1000

    Added test proj

---
+++ test/Android/MainActivity.cs
@@ -0,0 +1,116 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Text.Method;
+using Android.Widget;
+using Everwealth.OidcClient;
+using IdentityModel.OidcClient;
+using System;
+
+namespace AndroidTestApp
+{
+    [Activity(Label = "Android OIDC Test", MainLauncher = true, Icon = "@drawable/icon", LaunchMode = Android.Content.PM.LaunchMode.SingleTask)]
+    [IntentFilter(
+        new[] { Intent.ActionView },
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+        DataScheme = "",
+        DataHost = "",
+        DataPathPrefix = "")]
+    public class MainActivity : AuthClientActivity
+    {
+        private AuthClient _authClient;
+        private Action<string> writeLine;
+        private Action clearText;
+        private TextView _userDetailsTextView;
+        private string accessToken;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            _authClient = new AuthClient(new OidcClientOptions
+            {
+                //Authority = Resources.GetString(Resource.String.auth0_domain),
+                Authority = "",
+                ClientId = "",
+                Scope = "",
+                RedirectUri = "",
+                Browser = new WebViewBrowser(this, new string[] { "/signin", "/" })
+            }, this);
+
+            SetContentView(Resource.Layout.Main);
+            FindViewById<Button>(Resource.Id.LoginButton).Click += LoginButtonOnClick;
+            FindViewById<Button>(Resource.Id.LogoutButton).Click += LogoutButtonOnClick;
+            FindViewById<Button>(Resource.Id.UserButton).Click += UserButtonOnClick;
+
+            _userDetailsTextView = FindViewById<TextView>(Resource.Id.UserDetailsTextView);
+            _userDetailsTextView.MovementMethod = new ScrollingMovementMethod();
+
+            writeLine = (s) => _userDetailsTextView.Text += s + "\n";
+            clearText = () => _userDetailsTextView.Text = "";
+        }
+
+        private async void LoginButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            clearText();
+            writeLine("Starting login...");
+
+            var loginResult = await _authClient.LoginAsync();
+
+            if (loginResult.IsError)
+            {
+                writeLine($"An error occurred during login: {loginResult.Error}");
+                return;
+            }
+
+            accessToken = loginResult.AccessToken;
+
+            writeLine($"id_token: {loginResult.IdentityToken}");
+            writeLine($"access_token: {loginResult.AccessToken}");
+            writeLine($"refresh_token: {loginResult.RefreshToken}");
+
+            writeLine($"name: {loginResult.User.FindFirst(c => c.Type == "name")?.Value}");
+            writeLine($"email: {loginResult.User.FindFirst(c => c.Type == "email")?.Value}");
+
+            foreach (var claim in loginResult.User.Claims)
+            {
+                writeLine($"{claim.Type} = {claim.Value}");
+            }
+        }
+
+        private async void LogoutButtonOnClick(object sender, EventArgs e)
+        {
+            clearText();
+            writeLine("Starting logout...");
+
+            var result = await _authClient.LogoutAsync();
+            accessToken = null;
+            writeLine($"Logout successful: {result}");
+        }
+
+        private async void UserButtonOnClick(object sender, EventArgs e)
+        {
+            clearText();
+
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                writeLine("You need to be logged in to get user info");
+                return;
+            }
+
+            writeLine("Getting user info...");
+            var userInfoResult = await _authClient.GetUserInfoAsync(accessToken);
+
+            if (userInfoResult.IsError)
+            {
+                writeLine($"An error occurred getting user info: {userInfoResult.Error}");
+                return;
+            }
+
+            foreach (var claim in userInfoResult.Claims)
+            {
+                writeLine($"{claim.Type} = {claim.Value}");
+            }
+        }
+    }
+}
---
+++ test/iOS/MyViewController.cs
@@ -0,0 +1,105 @@
+﻿using Everwealth.OidcClient;
+using IdentityModel.OidcClient;
+using System;
+using UIKit;
+
+namespace iOSTestApp
+{
+    public partial class MyViewController : UIViewController
+    {
+        private AuthClient _authClient;
+        private Action<string> writeLine;
+        private Action clearText;
+        private string accessToken;
+
+        public MyViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            _authClient = new AuthClient(new OidcClientOptions
+            {
+                Flow = OidcClientOptions.AuthenticationFlow.AuthorizationCode,
+                ResponseMode = OidcClientOptions.AuthorizeResponseMode.Redirect,
+                Authority = "",
+                ClientId = "",
+                Scope = "",
+                RedirectUri = "",
+                Browser = new WKWebViewBrowser(new string[] { "/signin", "/" })
+            });
+
+            LoginButton.Clicked += Login;
+            UserInfoButton.Clicked += UserInfo;
+            LogoutButton.Clicked += Logout;
+
+            writeLine = (s) => TextView.Text += s + "\r\n";
+            clearText = () => TextView.Text = "";
+        }
+
+        private async void Login(object sender, EventArgs e)
+        {
+            clearText();
+            writeLine("Starting login...");
+
+            var loginResult = await _authClient.LoginAsync();
+
+            if (loginResult.IsError)
+            {
+                writeLine($"An error occurred during login: {loginResult.Error}");
+                return;
+            }
+
+            accessToken = loginResult.AccessToken;
+
+            writeLine($"id_token: {loginResult.IdentityToken}");
+            writeLine($"access_token: {loginResult.AccessToken}");
+            writeLine($"refresh_token: {loginResult.RefreshToken}");
+
+            writeLine($"name: {loginResult.User.FindFirst(c => c.Type == "name")?.Value}");
+            writeLine($"email: {loginResult.User.FindFirst(c => c.Type == "email")?.Value}");
+
+            foreach (var claim in loginResult.User.Claims)
+            {
+                writeLine($"{claim.Type} = {claim.Value}");
+            }
+        }
+
+        private async void Logout(object sender, EventArgs e)
+        {
+            clearText();
+            writeLine("Starting logout...");
+
+            var result = await _authClient.LogoutAsync();
+            accessToken = null;
+            writeLine($"Logout successful: {result}");
+        }
+
+        private async void UserInfo(object sender, EventArgs e)
+        {
+            clearText();
+
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                writeLine("You need to be logged in to get user info");
+                return;
+            }
+
+            writeLine("Getting user info...");
+            var userInfoResult = await _authClient.GetUserInfoAsync(accessToken);
+
+            if (userInfoResult.IsError)
+            {
+                writeLine($"An error occurred getting user info: {userInfoResult.Error}");
+                return;
+            }
+
+            foreach (var claim in userInfoResult.Claims)
+            {
+                writeLine($"{claim.Type} = {claim.Value}");
+            }
+        }
+    }
+}
